63ff41a047881e3f8cd2530dbacb1cf3cdfcec14
Westar35 <170506694+Westar35@users.noreply.github.com>
2026-07-18 17:19:44 +0300

    repository.go sort

--- internal/domain/aggregate/domain/repository.go
+++ internal/domain/aggregate/domain/repository.go
@@ -8,8 +8,8 @@ import (
 )
 
 type Repository interface {
-	Delete(ctx context.Context, domainName domain_aggregate_domain_model_domain_name.DomainName) error
 	Get(ctx context.Context, domainName domain_aggregate_domain_model_domain_name.DomainName) (*Domain, error)
 	ListByAccount(ctx context.Context, accountID domain_model_account_id.AccountID) ([]*Domain, error)
+	Delete(ctx context.Context, domainName domain_aggregate_domain_model_domain_name.DomainName) error
 	Save(ctx context.Context, domain *Domain) error
 }
